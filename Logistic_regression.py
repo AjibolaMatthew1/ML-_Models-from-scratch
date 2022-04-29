34546f517474c059366d6eb44cd110f51d50055c
Ajibola <52489772+AjibolaMatthew1@users.noreply.github.com>
2022-04-29 12:29:32 +0100

    Create Logistic_regression.py

---
+++ Logistic_regression.py
@@ -0,0 +1,40 @@
+import numpy as np
+from sklearn import linear_model
+
+def sigmoid(X):
+  ### Helper function to calculate sigmoid###
+    return 1 / (1 + np.exp(-X))
+
+class LogisticRegression:
+    def __init__(self, lr=0.001, n_iters=1000):
+        self.lr = lr
+        self.n_iters = n_iters
+        self.weights = None
+        self.bias = None
+        
+
+    def fit(self, X, y):
+        ###initializing weights and implementing gradient descent in this function###
+        n_samples, n_features = X.shape
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+
+        for _ in range(self.n_iters):
+            linear_model = np.dot(X, self.weights) + self.bias
+            y_predicted = sigmoid(linear_model)
+            
+            # Y = WX + b , where w is weights and b is bias. 
+            dw = (1 / n_samples) * np.dot(X.T, (y_predicted - y))
+            db = (1 / n_samples) * np.sum(y_predicted - y)
+
+            self.weights -= self.lr * dw
+            self.bias -= self.lr * db
+
+    def predict(self, X):
+        linear_model = np.dot(X, self.weights) + self.bias
+        y_predicted = sigmoid(linear_model)
+
+        y_predicted_cls = [1 if i > 0.5 else 0 for i in y_predicted]
+
+        return y_predicted_cls
+            
